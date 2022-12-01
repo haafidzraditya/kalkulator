91a735375fb5052826be3eaaa8268502657f9e0d
haafidzraditya <118706652+haafidzraditya@users.noreply.github.com>
2022-12-01 16:15:14 +0700

    Create kalkulator.js

---
+++ kalkulator.js
@@ -0,0 +1,14 @@
+var panggilnamainput = require("readline-sync");
+
+var bilangan1 = panggilnamainput.questionInt('masukan nilai 1.tambah , 2.kurang , 3.bagi , 4.kali = ');
+
+if  (bilangan1 == 1){
+    var masukanBilangan1 = panggilnamainput.questionInt('masukan bilangan1=');
+    var masukanBilangan2 = panggilnamainput.questionInt('masukan bilangan2=');
+    console.log(Number(masukanBilangan1)+(Number(masukanBilangan2)))
+}else {
+    if  (bilangan1 == 2){
+        var masukanBilangan1 = panggilnamainput.questionInt('masukan bilangan1=');
+        var masukanBilangan2 = panggilnamainput.questionInt('masukan bilangan2=');
+        console.log(Number(masukanBilangan1)-(Number(masukanBilangan2)))
+}
